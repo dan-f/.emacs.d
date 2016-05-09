557767943cea06b03af4b8911a24a682ca3e6aaa
Daniel Friedman <dfriedman58@gmail.com>
2016-05-09 09:43:42 -0400

    I change appearance too often to track in git...

--- config/init-appearance.el
+++ config/init-appearance.el
@@ -1,14 +1,6 @@
 (setq custom-safe-themes t)
 
-(use-package noctilux-theme
-  :ensure t
-  :config (load-theme 'noctilux))
-
 (use-package helm-themes
   :ensure t)
-
-(use-package smart-mode-line
-  :ensure t
-  :config (smart-mode-line-enable))
 
 (provide 'init-appearance)
