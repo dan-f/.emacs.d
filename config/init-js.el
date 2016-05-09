884c9b422f41a8c7cab22da76ed257346f5ba6be
Daniel Friedman <dfriedman58@gmail.com>
2016-05-09 09:45:41 -0400

    Stop highlighting common JS globals

--- config/init-js.el
+++ config/init-js.el
@@ -1,6 +1,12 @@
+(defconst jasmine-names '("jasmine" "beforeEach" "afterEach" "it" "expect" "spyOn" "setFixtures"))
+(defconst amd-names '("define" "require" "describe"))
+(defconst sinon-names '("sinon"))
+(defconst i18n-naames '("gettext"))
+
 (use-package js2-mode
   :ensure t
-  :init (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
+  :init (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+  (setq js2-global-externs (append jasmine-names amd-names sinon-names i18n-naames)))
 
 (use-package js2-refactor
   :ensure t
