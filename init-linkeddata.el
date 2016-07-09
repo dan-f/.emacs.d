71f1d3a83827599d89b92b675a2cc3bd5a2cbbef
dan <dan@localhost.localdomain>
2016-07-09 15:20:39 -0400

    Fix linkeddata config

--- init-linkeddata.el
+++
@@ -1,9 +0,0 @@
-(use-package ttl-mode
-  :ensure t)
-
-(use-package sparql-mode
-  :ensure t
-  :config
-  (add-to-list 'auto-mode-alist '("\\.sparql$" . sparql-mode)))
-
-(provide 'init-linkeddata)
