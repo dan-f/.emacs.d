f5f9b14d3e441e5b8953510b21b6ce261f90aeda
Daniel Friedman <dfriedman58@gmail.com>
2016-07-10 17:41:38 -0400

    Add some keybindings for os x

--- config/init-osx.el
+++ config/init-osx.el
@@ -1,3 +1,9 @@
 (setq mac-option-modifier 'super)
 
+(global-set-key (kbd "s-s") 'save-buffer)
+(global-set-key (kbd "s-b") 'ido-switch-buffer)
+(global-set-key (kbd "s-p") 'projectile-switch-project)
+(global-set-key (kbd "s-f") 'projectile-find-file)
+(global-set-key (kbd "s-m") 'magit-status)
+
 (provide 'init-osx)
