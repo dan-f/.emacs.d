7c1783f5f6095b870121b0489024c0784c8c6313
Daniel Friedman <dfriedman58@gmail.com>
2016-07-10 17:41:57 -0400

    Update javascript config

--- config/init-js.el
+++ config/init-js.el
@@ -6,8 +6,9 @@
 (use-package js2-mode
   :ensure t
   :init
-  (setq js2-basic-offset 2)
   (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+  (setq js2-basic-offset 2)
+  (setq js2-strict-missing-semi-warning nil)
   (setq js2-global-externs (append jasmine-names amd-names sinon-names i18n-names)))
 
 (use-package js2-refactor
