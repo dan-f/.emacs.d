78cd8fdbca7a1777b7572c49b90845ffd9043aa2
Daniel Friedman <dfriedman58@gmail.com>
2016-08-12 10:31:47 -0400

    Fix js2 settings

--- config/init-js.el
+++ config/init-js.el
@@ -5,7 +5,7 @@
 
 (use-package js2-mode
   :ensure t
-  :init
+  :config
   (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
   (setq js2-basic-offset 2)
   (setq js2-strict-missing-semi-warning nil)
